1c9e14647bec9f936cd22e4d18b2fa2050bb583f
ysk <y.sawa4787@gmail.com>
2018-10-06 22:54:24 +0900

    Update addToDoListとHTMLテンプレート
    --addToDoList--
    １、追加ボタンがクリックされた時にフォームの値が空だった場合はエラーメッセージを出力し、配列にデータを代入しないようにした。
    
    ２、テンプレート側でv-forを使用し、追加されたTodoの入力値が出力されるように修正
    
    Co-Authored-By: Yoshiki <heap-inc@users.noreply.github.com>

--- DevelopmentToDo/main.js
+++ DevelopmentToDo/main.js
@@ -2,17 +2,30 @@ var toDoApp = new Vue({
     //ルートテンプレートをマウントする
     el: '#toDoApp',
     data: {
-        mewItem: '',
+        newItem: '',
         todos: [],
+        errMsg: '',
+        toggle: false,
     },
     methods: {
         addToDoList: function(event) {
-            var todo = {
-                item: this.newItem
-            };
-            this.todos.push(todo);
-            //追加ボタンが押されるとフォームの中身がクリアされる
-            this.newItem = '';
+            if (this.newItem == '') {
+                //エラーメッセージに文字列を代入する
+                this.errMsg = 'タスク名を追加してください';
+                //テンプレートでエラーメッセージが表示されるようにする
+                this.toggle = true;
+            } else {
+                //エラー処理フラグをfalseに書き換えてエラーメッセージを非表示にする
+                this.toggle = false;
+                //追加ボタンが押される度に新しいオブジェクトを作成する
+                var todo = {
+                    item: this.newItem
+                };
+                // 配列todosに↑todoオブジェクトを追加
+                this.todos.push(todo);
+                //追加ボタンが押されるとフォームの中身がクリアされる
+                this.newItem = '';
+            }
         }
     },
 })
