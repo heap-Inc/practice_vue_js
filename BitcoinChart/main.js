6b53c5a7bb8421a6466a5a68270587c2a2ca2be0
ysk <y.sawa4787@gmail.com>
2018-10-07 09:53:44 +0900

    commit 初回コミット
    ビットコイン価格が表示できる状態

---
+++ BitcoinChart/main.js
@@ -0,0 +1,32 @@
+var btcChart = new Vue({
+    el: '#btcChart',
+    data: {
+        bpi: null,
+        hasErr: false,
+
+    },
+    //インスタンスがマウントされた後に呼ばれる
+    mounted: function() {
+        //axiosを使ってhttpリクエストを行う
+        axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(function(response){
+            //↓Jsonの階層をおって行くとレートにたどり着く
+            // console.log(response.data.bpi.USD.rate_float);
+
+            // dataプロパティのbpiにdataオブジェクトを代入する
+            this.bpi = response.data.bpi;
+        }.bind(this))
+        //エラーをキャッチできるようにする
+        .catch(function(error){
+            console.log(error);
+            this.hasErr = true;
+        }.bind(this))
+    },
+    //小数点を二桁にするフィルターを実装する
+    filters: {
+        currencyDecimal(value) {
+            return value.toFixed(2);
+        }
+    }
+
+})
